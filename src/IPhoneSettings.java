ebebb65b507f39739bfafc2a6fc1bd1b52bf995c
jannakam <jannakalmuqaisib@gmail.com>
2024-11-05 13:25:53 +0300

    task done

--- src/IPhoneSettings.java
+++ src/IPhoneSettings.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
-
 public class IPhoneSettings {
+
+    //add some properties for the custom methods
+
+
+    //let's say brightness is a percentage from 0-100
+    private int brightness;
+    //let's say volume is a percentage from 0-100
+    private int volume;
+    private boolean wifi;
+
+    //private constructor
+    private IPhoneSettings() {
+    }
+
+    //private static variable for single instance
+    private static IPhoneSettings instance;
+
+    //getinstance method
+    public static IPhoneSettings getInstance() {
+        if (instance == null) {
+            instance = new IPhoneSettings();
+        }
+        return instance;
+    }
+
+    //getters
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public boolean getWifiStatus() {
+        return wifi;
+    }
+
+    //setters
+    public void setBrightness(int brightness) {
+        if (brightness >=0 && brightness <= 100) {
+            this.brightness = brightness;
+        }
+    }
+
+    public void setVolume(int volume) {
+        if (volume >=0 && volume <= 100) {
+            this.volume = volume;
+        }
+        else {
+            this.volume = 0;
+        }
+    }
+
+    public void invertWifiStatus(boolean wifi) {
+        this.wifi = !wifi;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //create an instance
+        IPhoneSettings settings = IPhoneSettings.getInstance();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //testing brightness
+        int brightness = settings.getBrightness();
+        System.out.println(brightness);
+        settings.setBrightness(80);
+        System.out.println(settings.getBrightness());
+
+        //testing volume
+        settings.setVolume(-3);
+        System.out.println(settings.getVolume());
+        settings.setVolume(30);
+        System.out.println(settings.getVolume());
+
+        //testing wifi
+        settings.invertWifiStatus(true);
+        boolean wifiStatus = settings.getWifiStatus();
+        System.out.println(wifiStatus);
     }
 }
